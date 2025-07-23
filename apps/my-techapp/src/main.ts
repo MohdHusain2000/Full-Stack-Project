d7f3e9531ccaaea6364e07fe489341a46be4f5ca
Mohamed Husain <mo.abdulla3000@gmail.com>
2025-07-23 15:40:25 -0700

    fix the main ts

--- apps/my-techapp/src/main.ts
+++ apps/my-techapp/src/main.ts
@@ -6,10 +6,14 @@ import { authReducer } from './app/states/auth/reducer/users.reducer';
 import { provideHttpClient } from '@angular/common/http';
 import { UserEffects } from './app/states/auth/effect/users.effect';
 import { provideEffects } from '@ngrx/effects';
+import { productReducer } from './app/states/product-details/reducer/app.reducer';
 
 bootstrapApplication(App, {
   providers: [
-    provideStore({ auth: authReducer }),
+    provideStore({
+      auth: authReducer,
+      products: productReducer
+    }),
     provideEffects([UserEffects]),
     provideHttpClient(),
     ...appConfig.providers
